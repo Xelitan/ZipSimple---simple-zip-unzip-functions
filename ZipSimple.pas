7d2f6e4e8c630635221501fef68df00679dd47e8
Xelitan <140884394+Xelitan@users.noreply.github.com>
2026-05-23 09:32:42 +0200

    Add files via upload

--- ZipSimple.pas
+++ ZipSimple.pas
@@ -20,19 +20,244 @@ type
     procedure CreateOut(Sender: TObject; var AStream: TStream; AItem: TFullZipFileEntry);
     procedure DoneOut(Sender: TObject; var AStream: TStream; AItem: TFullZipFileEntry);
   end;
+  
+  function DateTimeToUnix(Time: TDateTime): Cardinal;
 
   //public API
-  function Zip(Uncompressed: RawByteString; Filename: String): RawByteString;
-  function UnZip(Compressed: RawByteString; Filename: String): RawByteString;
-  function Deflate(Uncompressed: RawByteString): RawByteString; //gzdeflate from PHP
-  function Inflate(Compressed: RawByteString): RawByteString; //gzinflate from PHP
+  function Zip(const Uncompressed: RawByteString; const Filename: String): RawByteString;
+  function UnZip(const Compressed: RawByteString; const Filename: String): RawByteString;
+  function Deflate(const Uncompressed: RawByteString): RawByteString; //gzdeflate from PHP
+  function Inflate(const Compressed: RawByteString): RawByteString; //gzinflate from PHP
 
-  function Zip(const Buf; BufZize: SizeUInt; Filename: String; var OutBuf): SizeUInt; overload;
+  function UnGzip(const Compressed: String): String; //gzdecode from PHP
+  function GZip(const Uncompressed: AnsiString): AnsiString;
+  function Zlib(const UnCompressed: String): String; //gzcompress from PHP
+  function UnZlib(const Compressed: String): String; //gzdecompress from PHP
+
+  function Zip(const Buf; BufZize: SizeUInt; const Filename: String; var OutBuf): SizeUInt; overload;
 
 implementation
 
+function DateTimeToUnix(Time: TDateTime): Cardinal;
+const
+  UnixStartDate: TDateTime = 25569.0; // 1970-01-01
+begin
+  Result := Round((Time - UnixStartDate) * 86400);
+end;
 
-function Inflate(Compressed: RawByteString): RawByteString; //gzinflate from PHP
+function UnZlib(const Compressed: String): String; //gzdecompress from PHP
+const BuffSize = 4096;
+var Z: TDecompressionStream;
+    Read: Integer;
+    Temp: String;
+    Str: TStringStream;
+begin
+  Str := TStringStream.Create(Compressed);
+  Z := TDecompressionStream.Create(Str, False);
+  SetLength(Temp, BuffSize);
+  Result := '';
+ 
+  try
+    repeat
+      Read := Z.Read(Temp[1], BuffSize);
+      Result := Result + Copy(Temp, 1, Read);
+    until Read<BuffSize-1;
+  finally
+    Z.Free;
+    Str.Free;
+  end;
+end;
+ 
+
+function Zlib(const UnCompressed: String): String; //gzcompress from PHP
+const BuffSize = 4096;
+var Z: TCompressionStream;
+    Read: Integer;
+    Temp: AnsiString;
+    Str: TStringStream;
+    Str2: TStringStream;
+begin
+  Str := TStringStream.Create('');
+  Str2 := TStringStream.Create(UnCompressed);
+ 
+  Z := TCompressionStream.Create(clMax, Str, False);
+ 
+  SetLength(Temp, BuffSize);
+  Result := '';
+ 
+  try
+    repeat
+      Read := Str2.Read(Temp[1], BuffSize);
+      Z.Write(Temp[1], Read);
+ 
+    until Read = 0;
+  finally
+    Z.Free;
+    Str2.Free;
+  end;
+ 
+  Result := Str.DataString;
+  Str.Free;
+end;
+ 
+
+function GZip(const Uncompressed: AnsiString): AnsiString;
+ 
+  procedure AppendByte(var S: AnsiString; B: Byte);
+  var
+    L: Integer;
+  begin
+    L := Length(S);
+    SetLength(S, L + 1);
+    S[L + 1] := AnsiChar(B);
+  end;
+ 
+  procedure AppendUInt32LE(var S: AnsiString; Value: Cardinal);
+  begin
+    AppendByte(S, Byte(Value and $FF));
+    AppendByte(S, Byte((Value shr 8) and $FF));
+    AppendByte(S, Byte((Value shr 16) and $FF));
+    AppendByte(S, Byte((Value shr 24) and $FF));
+  end;
+ 
+  function CRC32Of(const S: AnsiString): Cardinal;
+  const
+    Polynomial = $EDB88320;
+  var
+    CRC: Cardinal;
+    I, J: Integer;
+    B: Byte;
+  begin
+    CRC := $FFFFFFFF;
+ 
+    for I := 1 to Length(S) do
+    begin
+      B := Byte(S[I]);
+      CRC := CRC xor B;
+      for J := 0 to 7 do
+      begin
+        if (CRC and 1) <> 0 then
+          CRC := (CRC shr 1) xor Polynomial
+        else
+          CRC := CRC shr 1;
+      end;
+    end;
+ 
+    Result := not CRC;
+  end;
+ 
+var
+  Deflated: TStringStream;
+  Input: TStringStream;
+  Z: TCompressionStream;
+  CRC: Cardinal;
+  UnixTime: Cardinal;
+begin
+  Deflated := TStringStream.Create('');
+  try
+    Input := TStringStream.Create(String(Uncompressed));
+    try
+      Z := TCompressionStream.Create(clMax, Deflated, True);
+      try
+        Z.CopyFrom(Input, 0);
+      finally
+        Z.Free;
+      end;
+ 
+      CRC := CRC32Of(Uncompressed);
+      UnixTime := DateTimeToUnix(Now);
+ 
+      Result := '';
+ 
+      // GZIP header (10 bytes)
+      AppendByte(Result, $1F);          // ID1
+      AppendByte(Result, $8B);          // ID2
+      AppendByte(Result, $08);          //CM = deflate
+      AppendByte(Result, $00);          // FLG = no extra fields
+      AppendUInt32LE(Result, UnixTime); // ModTime
+      AppendByte(Result, $02);          // XFL = max compression
+      AppendByte(Result, $FF);          // OS = unknown
+ 
+      Result := Result + AnsiString(Deflated.DataString);
+ 
+      // Gzip foot
+      AppendUInt32LE(Result, CRC);                      // CRC32 of unpacked data
+      AppendUInt32LE(Result, Cardinal(Length(Uncompressed))); // ISIZE mod 2^32
+    finally
+      Input.Free;
+    end;
+  finally
+    Deflated.Free;
+  end;
+end;
+ 
+function UnGzip(const Compressed: String): String; //gzdecode from PHP
+const BuffSize = 4096;
+var Z: TDecompressionStream;
+    Read: Integer;
+    Temp: String;
+    Str: TStringStream;
+ 
+    ID: Word;
+    Method: Byte;
+    Head: Word;
+    Date: Cardinal;
+    Flag, OS: Byte;
+    Skip: Word;
+    Tmp: String;
+    Null: Integer;
+begin
+  Str := TStringStream.Create(Compressed);
+ 
+  ID := Str.ReadWord;
+  Method := Str.ReadByte;
+  Head := Str.ReadByte;
+  Date := Str.ReadDWord;
+  Flag := Str.ReadByte;
+  OS := Str.ReadByte;
+ 
+  //FEXTRA
+  if ((Head shr 2) and 1 = 1) then begin
+    Skip := Str.ReadByte;
+    Str.Position := Str.Position + Skip;
+  end;
+ 
+  //FNAME
+  if ((Head shr 3) and 1 = 1) then begin
+    Tmp := Str.ReadString(256);
+    Null := Pos(chr(0), Tmp);
+    Str.Position := Str.Position - Length(Tmp) + Null;
+  end;
+ 
+  //FCOMMENT
+  if ((Head shr 4) and 1 = 1) then begin
+    Tmp := Str.ReadString(256);
+    Null := Pos(chr(0), Tmp);
+    Str.Position := Str.Position - Length(Tmp) + Null;
+  end;
+ 
+  //FHCRC
+  if ((Head shr 1) and 1 = 1) then begin
+    Str.Position := Str.Position + Skip;
+  end;
+ 
+  //https://datatracker.ietf.org/doc/html/rfc1952#page-5
+  Z := TDecompressionStream.Create(Str, True);
+  SetLength(Temp, BuffSize);
+  Result := '';
+ 
+  try
+    repeat
+      Read := Z.Read(Temp[1], BuffSize);
+      Result := Result + Copy(Temp, 1, Read);
+    until Read<BuffSize-1;
+  finally
+    Z.Free;
+    Str.Free;
+  end;
+end;
+
+function Inflate(const Compressed: RawByteString): RawByteString; //gzinflate from PHP
 const BuffSize = 4096;
 var Z: TDecompressionStream;
     Read: Integer;
@@ -55,7 +280,7 @@ begin
   end;
 end;
 
-function Deflate(Uncompressed: RawByteString): RawByteString; //gzdeflate from PHP
+function Deflate(const Uncompressed: RawByteString): RawByteString; //gzdeflate from PHP
 const BuffSize = 4096;
 var Z: TCompressionStream;
     Read: Integer;
@@ -80,7 +305,7 @@ end;
 
 
 
-function Zip(const Buf; BufZize: SizeUInt; Filename: String; var OutBuf): SizeUInt;
+function Zip(const Buf; BufZize: SizeUInt; const Filename: String; var OutBuf): SizeUInt;
 
   procedure AddWord(var S: RawByteString; W: Word);
   begin
@@ -220,8 +445,7 @@ begin
   S.Position := 0;
 end;
 
-function Zip(Uncompressed: RawByteString;
-  Filename: String): RawByteString;
+function Zip(const Uncompressed: RawByteString; const Filename: String): RawByteString;
 var
   Z        : TZipper;
   InStream : TMemoryStream;
@@ -245,8 +469,7 @@ begin
   end;
 end;
 
-function UnZip(Compressed: RawByteString;
-  Filename: String): RawByteString;
+function UnZip(const Compressed: RawByteString; const Filename: String): RawByteString;
 var
   U : TUnZipper;
   H : TZipMemHelper;
